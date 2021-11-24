e9572f7c8330312a4d065d3d9a873a9d55c5922a
Aaron Shaji <90400808+Termin-a-t-o-r@users.noreply.github.com>
2021-11-24 16:05:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -37,12 +37,10 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('🎬 Channel', url='https://t.me/Film_Arcade_Official'),
+            InlineKeyboardButton('♀️ Group', url='https://t.me/Film_Arcade_Chat')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
-            ],[
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('⭕ About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -77,12 +75,10 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('🎬 Channel', url='https://t.me/Film_Arcade_Official'),
+            InlineKeyboardButton('♀️ Group', url='https://t.me/Film_Arcade_Chat')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
-            ],[
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('⭕ About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
